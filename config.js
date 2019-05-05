b9620084d9198b404ea62ca389df18436018a266
741boss741 <47608727+741boss741@users.noreply.github.com>
2019-05-05 15:31:00 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    TOKEN: 'NTcyODQzMTcyMDk1NTkwNDEx.XMtjgQ.8LpnQSAPctSn1Y56zlN-dy3f4GY',    
+
+    YT_API_KEY: 'api ',
+
+    prefix: '-',
+
+    devs: ['493103326179622933']
+
+}
+
+
+    
+ 
